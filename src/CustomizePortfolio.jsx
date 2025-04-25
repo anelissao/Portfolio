7878786ba512edd83335879b481d728100f1fa37
Anouar <anouarelissaoui20@gmail.com>
2025-04-25 11:54:14 +0100

    initing my portfolio

---
+++ src/CustomizePortfolio.jsx
@@ -0,0 +1,101 @@
+import Bootstrap from "./assets/Bootstrap";
+import Git from "./assets/Git";
+import JavaScript from "./assets/JavaScript";
+import NextJs from "./assets/NextJs";
+import React from "./assets/React";
+import ReactQuery from "./assets/ReactQuery";
+import TailwindCSS from "./assets/TailwindCSS";
+import CSS from "./assets/CSS";
+import HTML from "./assets/HTML";
+
+// Your Skills
+export const skills = [
+  {
+    name: "React",
+    icon: <React />,
+  },
+  {
+    name: "NextJs",
+    icon: <NextJs />,
+  },
+  {
+    name: "JavaScript",
+    icon: <JavaScript />,
+  },
+  {
+    name: "React Query",
+    icon: <ReactQuery />,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: <TailwindCSS />,
+  },
+  {
+    name: "Git",
+    icon: <Git />,
+  },
+  {
+    name: "Bootstrap",
+    icon: <Bootstrap />,
+  },
+  {
+    name: "CSS",
+    icon: <CSS />,
+  },
+  {
+    name: "HTML",
+    icon: <HTML />,
+  },
+];
+
+// Your Projects
+export const projects = [
+  {
+    title: "Nostalchive",
+    description:
+      "An online store for buying ready-made or custom movie and series collections on external hard drives.",
+    year: "2025",
+    technologies: "NextJs, Tailwind, TanStack",
+    image: "./nostalchive.png",
+    demoLink: "https://nostalchive.com",
+    // githubLink: "https://github.com/amirmousav1/NEXTON-eCommerce",
+  },
+  {
+    title: "MagnetMovie",
+    description:
+      "MagnetMovie is a powerful tool to search for your favorite movies and download their torrent files or copy magnet links to start downloading effortlessly.",
+    year: "2024",
+    technologies: "React, React Router, TanStack, Tailwind",
+    image: "./magnetmovie.png",
+    demoLink: "https://magnetmovie.netlify.app",
+    githubLink: "https://github.com/amirmousav1/MagnetMovie",
+  },
+  {
+    title: "TickBoard",
+    description:
+      "Tickboard is a sleek and intuitive to-do application designed to streamline task management. With features like priority-based sorting, real-time task updates, and local storage persistence, Tickboard empowers users to stay organized and productive effortlessly.",
+    year: "2024",
+    technologies: "React, Tailwind",
+    image: "./tickboard.png",
+    demoLink: "https://tickboard.netlify.app",
+    githubLink: "https://github.com/amirmousav1/TickBoard",
+  },
+];
+
+// Your Informations
+export const myInformations = {
+  fullName: "Anouar Elissaoui",
+  myPhoto: "/Anouar.jpeg",
+  bio: "A Full-Stack Web Developer Passionate About Creating Seamless User Experiences and Building Scalable Architectures Through the Power of Computer Science.",
+  myEmail: "anouarelissaoui20@gmail.com",
+  linkedinUrl: "https://www.linkedin.com/in/anouar-elissaoui-785752227/",
+  githubUrl: "https://github.com/anelissao",
+  telegramUrl: "https://t.me/shomabeguamir",
+  resumeUrl:
+    "https://drive.google.com/file/d/1GabP_oSU6CqOsPJ5J94rpT6I3Vd7fulQ/view?usp=sharing",
+  aboutTitle: "I am a Full Stack Web Developer based in Morocco",
+  aboutMe:
+    "Hi there! I'm a 25-year-old Full Stack developer from Morocco, studying IT. <br /> I pay close attention to details and always try to deliver the best results. Solving problems excites me, and I'm always motivated to learn new things. <br /> When I'm not coding, I enjoy playing chess, lestening to music, and catching up on tech updates. <br /> Feel free to reach out if you'd like to collaborate on something great!",
+};
+
+export const formspreeEndpoint = "mzzbwlbg";
